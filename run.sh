c97951ae1d470ac6d55f2cd8cb924a71fc98bba4
James <james.colton@gmail.com>
2018-04-14 02:32:50 +0100

    fix filename

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 cd /var/www/html
 source .env
-sed -- "s/___HOSTNAME___/$CFG_HOSTNAME/g;" docker-compose.yml.tmpl > docker-compose.yml
+sed -- "s/___HOSTNAME___/$CFG_HOSTNAME/g;" docker-compose.tmpl.yml > docker-compose.yml
 docker-compose rm -f
 git pull
 docker-compose up --force-recreate
