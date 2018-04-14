7f1570a0d05ced204724a006379f7086ade61ff3
James <james.colton@gmail.com>
2018-04-14 03:43:57 +0100

    update

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@
 cd /var/www/html
 source .env
 sed -- "s/___HOSTNAME___/$CFG_HOSTNAME/g;" docker-compose.tmpl.yml > docker-compose.yml
+touch /tmp/acme.json
 docker-compose rm -f
 git pull
 docker-compose up --force-recreate
